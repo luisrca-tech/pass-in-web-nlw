ac8666820ffca67d4d8868e121c3b4171bc3b8ca
Luis Felipe <luisrochacruzalves@gmail.com>
2024-04-02 23:47:04 -0300

    feat: create table-cell extending td props

---
+++ src/components/table/table-cell.tsx
@@ -0,0 +1,10 @@
+import { ComponentProps } from "react";
+import {twMerge} from 'tailwind-merge'
+
+interface TableCellProps extends ComponentProps<'td'> {}
+
+export function TableCell({...rest}: TableCellProps) {
+  return (
+    <td {...rest} className={twMerge("py-3 px-4 text-sm text-zinc-300", rest.className)}></td>
+  )
+}
