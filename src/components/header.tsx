48c6b10afe090700ac8d1c4deaac286e106fd101
Luis Felipe <luisrochacruzalves@gmail.com>
2024-04-02 23:45:28 -0300

    feat: add navLink component to anchors

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,4 +1,5 @@
 import NLW from '../assets/nlw.svg'
+import { NavLink } from './nav-lik'
 
 export function Header() {
   return (
@@ -6,8 +7,8 @@ export function Header() {
       <img src={NLW} alt="" />
 
       <nav className="flex items-center gap-5">
-        <a href="" className="font-medium text-sm text-zinc-300">Eventos</a>
-        <a href="" className="font-medium text-sm">Participantes</a>
+        <NavLink href='/eventos' >Eventos</NavLink>
+        <NavLink href='/participantes' >Participantes</NavLink>
       </nav>
     </div>
   )
