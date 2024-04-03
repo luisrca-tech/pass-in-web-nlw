5e0910527577bfe62cd0d5fde84ccafc4ebaf3e1
Luis Felipe <luisrochacruzalves@gmail.com>
2024-04-02 23:46:25 -0300

    refactor: delete commentary

--- src/components/icon-button.tsx
+++ src/components/icon-button.tsx
@@ -8,10 +8,6 @@ interface IconButtonProps extends ComponentProps<'button'> {
 export function IconButton({transparent, ...rest}: IconButtonProps) {
   return (
     <button {...rest} 
-    // className={transparent 
-    //   ? "bg-black/20 border border-white/10 rounded-md p-1.5" 
-    //   : "bg-white/10 border border-white/10 rounded-md p-1.5" 
-    // }
     className={twMerge(
       'border border-white/10 rounded-md p-1.5',
        transparent ? 'bg-black/20' : 'bg-white/10',
